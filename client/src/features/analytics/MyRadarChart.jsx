09b5b2abbe32515e96a8b953020bd8b8ab554f79
Abhishek Kumar <abhishekgkp567856@gmail.com>
2025-09-23 22:56:13 +0530

    test commit

--- client/src/features/analytics/MyRadarChart.jsx
+++ client/src/features/analytics/MyRadarChart.jsx
@@ -43,7 +43,7 @@ const data = [
 const MyRadarChart = () => {
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+            <RadarChart cx="50%"cy="50%" outerRadius="80%" data={data}>
                 <PolarGrid />
                 <PolarAngleAxis dataKey="subject" />
                 <PolarRadiusAxis />
