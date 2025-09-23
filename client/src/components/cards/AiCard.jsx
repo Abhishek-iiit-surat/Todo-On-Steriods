694397717b3fcd0249bb81edf0b884e991981418
Abhishek Kumar <84232305+Abhishek-iiit-surat@users.noreply.github.com>
2025-09-23 22:50:26 +0530

    Revert "feat: added area graph in activity card"

--- client/src/components/cards/AiCard.jsx
+++ client/src/components/cards/AiCard.jsx
@@ -6,7 +6,7 @@ function AiCard() {
     // Split into words, take first 10, join back & add ellipsis
     const truncated = content.split(" ").slice(0, 10).join(" ") + "...";
     return (
-        <div className="bg-white flex-col rounded-3xl p-6 h-full shadow">
+        <div className="bg-white flex-col rounded-xl p-6 h-full shadow">
             <div className="flex items-center justify-between mb-4">
                 <h3 className="text-md font-bold">What AI is saying...</h3>
                 <FaWandMagicSparkles className='w-5 h-5 text-violet-500 font-semibold' />
